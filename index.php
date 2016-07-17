b2086ad7afd2f917833d03077e903fa86f52b6a3
Harald Niemeczek <harald@niemeczek.at>
2016-07-17 04:06:10 +0200

    corrected .gitignore

--- index.php
+++ index.php
@@ -38,20 +38,9 @@
 				</div>
 	
 		</section>
-		<footer class="tabellen_zelle" id="footer_height">
-				<div id="testfooter"></div>
-			
-				<nav id="steuerung_footer">
-					<ul>
-						<li><a href="#">Impressum</a></li>
-						<li><a href="#">Datenschutzerklärung</a></li>
-						<li><a href="#">Disclaimer</a></li>
-						<li><a href="#">Lorem Ipsum</a></li>
-						<li><a href="#">Lorem Ipsum</a></li>				
-					</ul>
-				</nav>
-					
-		</footer>
+		<?php 
+			include '/template/footer/footer.php';
+		?>	
 	</body>
 </html>
 
---
+++ template/footer/footer.php
@@ -0,0 +1,14 @@
+		<footer class="tabellen_zelle" id="footer_height">
+				<div id="testfooter"></div>
+			
+				<nav id="steuerung_footer">
+					<ul>
+						<li><a href="#">Impressum</a></li>
+						<li><a href="#">Datenschutzerklärung</a></li>
+						<li><a href="#">Disclaimer</a></li>
+						<li><a href="#">Lorem Ipsum</a></li>
+						<li><a href="#">Lorem Ipsum</a></li>				
+					</ul>
+				</nav>
+					
+		</footer>
