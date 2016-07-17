8f3c5bfe5a916e61ae882794498f53393111e08f
Harald Niemeczek <harald@niemeczek.at>
2016-07-17 03:44:49 +0200

    first commit

---
+++ template/head/head.php
@@ -0,0 +1,7 @@
+			<meta charset="UTF-8" />
+			<title>
+				Harald Niemeczek
+			</title>
+			<meta name="description" content="Harald Niemeczek" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="style/style.css" type="text/css" rel="stylesheet" />		
---
+++ template/header/header.php
@@ -0,0 +1,23 @@
+		<header class="tabellen_zelle" id="nav-menue">
+							<a href="#" id="bereichlogo" class="linie_nach_unten_wenn_geoeffnet">Harald Niemeczek</a>
+				<nav id="steuerung">
+					<ul>
+						<li><a href="#">Über mich</a></li>
+						<li><a href="#">Fotografie</a></li>
+						<li><a href="#">Projekte</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Kontakt</a></li>				
+					</ul>
+				</nav>
+				<a class="menue-button menue-button-beschr-close" href="#">
+					<span class="menue-button-line"></span>
+					<span class="menue-button-line"></span>
+					<span class="menue-button-line"></span>
+				</a>
+				<a class="menue-button menue-button-beschr-open"  href="#nav-menue">
+					<span class="menue-button-line"></span>
+					<span class="menue-button-line"></span>
+					<span class="menue-button-line"></span>
+				</a>	
+				<div class="linie_nach_unten_wenn_geoeffnet"></div>
+		</header>
