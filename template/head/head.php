165c5653f7026ef0819e5d5d383d9d366644c962
Harald Niemeczek <harald@niemeczek.at>
2017-01-17 13:30:49 +0100

    test

--- template/head/head.php
+++ template/head/head.php
@@ -5,3 +5,8 @@
 			<meta name="description" content="Harald Niemeczek" />
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link href="/style/min-style.css" type="text/css" rel="stylesheet" />
+
+
+
+
+
